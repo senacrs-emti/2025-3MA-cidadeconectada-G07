589d675ec3c6174ac31594d07802811fd9d1e4de
Bianca Bulling <133228830+Biancapinz@users.noreply.github.com>
2025-12-03 07:41:41 -0300

    Reapply "finalizando o teste do maps"
    This reverts commit 28fbb71292c98fd06da0ef40e89ad01aa041fe4a.

---
+++ pagina.php
@@ -0,0 +1,36 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+// CONEXÃO COM O BANCO
+$conn = new mysqli("localhost", "root", "", "atividadeotem");
+
+if ($conn->connect_error) {
+    die("Erro: " . $conn->connect_error);
+}
+
+// pego o tipo da URL
+$tipo = $_GET['tipo'] ?? '';
+
+if ($tipo == '') {
+    die("Nenhum tipo enviado!");
+}
+
+// defino qual tabela acessar
+$tabelas_validas = ['moradia','alimentacao','postosaude','centropop'];
+
+if (!in_array($tipo, $tabelas_validas)) {
+    die("Tipo inválido!");
+}
+
+// buscar o primeiro registro da tabela
+$sql = "SELECT * FROM $tipo LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 0) {
+    die("Nenhum registro encontrado!");
+}
+
+$dados = $result->fetch_assoc();
+?>
