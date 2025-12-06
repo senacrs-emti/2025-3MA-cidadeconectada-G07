e9e8605dd1dc07676ee3f33730d82f481e122677
Bianca Bulling <133228830+Biancapinz@users.noreply.github.com>
2025-12-06 09:25:48 -0300

    fix Diretorios CSS

--- PHP/Moradia.php
+++ PHP/Moradia.php
@@ -50,7 +50,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Totem</title>
-    <link rel="stylesheet" href="verTodasMoradias.css">
+    <link rel="stylesheet" href="./verTodasMoradias.css">
 </head>
 
 <body>
